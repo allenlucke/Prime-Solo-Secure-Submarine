3160b4a4fe811cd7ffb30703bddfe480901c6e54
Allen Lucke <allenlucke@gmail.com>
2020-01-09 13:40:40 -0600

    basemode complete

--- server/routes/secrets.router.js
+++ server/routes/secrets.router.js
@@ -1,10 +1,18 @@
 const express = require('express');
 const pool = require('../modules/pool');
 const router = express.Router();
+const { rejectUnauthenticated } = require('../modules/authentication-middleware');
+const encryptLib = require('../modules/encryption');
+const userStrategy = require('../strategies/user.strategy');
 
-router.get('/', (req, res) => {
+router.get('/', rejectUnauthenticated, (req, res) => {
+    // res.send(req.user);
+    const userLevel = req.user.clearance_level
     console.log('req.user:', req.user);
-    pool.query('SELECT * FROM "secret";')
+    console.log(userLevel);
+    const queryString = `SELECT * FROM "secret"
+                        WHERE ${userLevel} >= "secret".secrecy_level;`;
+    pool.query(queryString)
         .then(results => res.send(results.rows))
         .catch(error => {
             console.log('Error making SELECT for secrets:', error);
